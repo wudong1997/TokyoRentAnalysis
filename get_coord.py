05dd140ea3b424597dc0a8ed04dd88f0e2fa22e7
WU DONG <62734556+wudong1997@users.noreply.github.com>
2021-08-30 13:51:06 +0900

    Update get_coord.py

--- get_coord.py
+++ get_coord.py
@@ -2,7 +2,7 @@ from numpy import nan
 import requests
 import pandas as pd
 
-KEY = 'AIzaSyByBXvK0ASTyfdDsOHiuayBIMGbvwirfQE'
+KEY = ''
 
 
 def get_coord(address, API_KEY=KEY):
@@ -45,18 +45,5 @@ def location(file):
 
     return df
 
-
-# fl = ['./p_data/n_itabashi.csv', './p_data/n_kita.csv', './p_data/n_meguro.csv', './p_data/n_nakano.csv',
-#       './p_data/n_nerimaku.csv', './p_data/n_oda.csv', './p_data/n_suginami.csv', './p_data/n_toshima.csv']
-#
-# aim = ['itabashi_loc', 'kita_loc', 'meguro_loc', 'nakano_loc',
-#       'nerimaku_loc', 'oda_loc', 'suginami_loc', 'toshima_loc']
-#
-# for i in range(len(fl)):
-#     df = location(fl[i])
-#     df.to_csv('{}.csv'.format(aim[i]), encoding='utf-8', index=0)
-#     print(i)
-
-
-df = location('./p_data/n_sedagaya.csv')
-df.to_csv('{}.csv'.format('sedagaya.loc'), encoding='utf-8', index=0)
+df = location('READFILENAME')
+df.to_csv('{}.csv'.format('WRITEFILENAME'), encoding='utf-8', index=0)
